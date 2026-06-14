f603d948920411710fa4f5a977386876168a7ffb
jcynay0206 <yuny1584@gmail.com>
2026-06-13 23:25:38 -0400

    Update _firebase.js

--- api/_firebase.js
+++ api/_firebase.js
@@ -15,7 +15,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+      privateKey: privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").split(String.fromCharCode(10)).join("\n").replace(/\\n/g, "\n"),
     }),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
