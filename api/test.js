1dc901ebe7bb31c9872e3857a4f5f0394470fb7d
jcynay0206 <yuny1584@gmail.com>
2026-07-04 09:52:54 -0400

    Delete api/test.js

--- api/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = function handler(req, res) {
-  res.status(200).json({ 
-    ok: true, 
-    key: process.env.STRIPE_SECRET_KEY ? "key exists" : "NO KEY",
-    method: req.method
-  });
-};
